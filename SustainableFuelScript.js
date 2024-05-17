8471089f2810537b9cb556574a43f3ae7278a3a3
saintvisionary <143473956+saintvisionary@users.noreply.github.com>
2024-05-17 13:06:55 -0400

    Add files via upload

---
+++ SustainableFuelScript.js
@@ -0,0 +1,4 @@
+// Add an event listener to the button to show an alert when clicked
+document.querySelector('button').addEventListener('click', function() {
+    alert('Learn more about our sustainable aviation solutions!');
+});
